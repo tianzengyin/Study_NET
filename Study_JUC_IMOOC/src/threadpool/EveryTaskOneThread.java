56d6a21a97c63e96f042a8358d52b9aef054a9d6
tian <tianzengyin123@gmail.com>
2020-05-24 08:50:39 +0800

    2020.05.24增加JUC练习相关内容,玩转JUC慕课网资料

---
+++ Study_JUC_IMOOC/src/threadpool/EveryTaskOneThread.java
@@ -0,0 +1,23 @@
+package threadpool;
+
+/**
+ * 描述：     TODO
+ */
+public class EveryTaskOneThread {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            Thread thread = new Thread(new Task());
+            thread.start();
+        }
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("执行了任务");
+        }
+    }
+
+}
---
+++ Study_JUC_IMOOC/src/threadpool/ForLoop.java
@@ -0,0 +1,26 @@
+package threadpool;
+
+import java.util.concurrent.Executors;
+
+/**
+ * 描述：     TODO
+ */
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            Thread thread = new Thread(new Task());
+            thread.start();
+        }
+        Executors.newFixedThreadPool(1);
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println("执行了任务");
+        }
+    }
+
+}
